ccc5df3fbdabb49b23d0c74b35a8cc94d12b1f5f
agmar28 <ag.marcelo28@gmail.com>
2020-10-01 12:06:46 +0800

    Removed Commented Code

--- public/js/app.js
+++ public/js/app.js
@@ -1,32 +1,3 @@
-console.log('client-sided javascript is loaded!');
-//  fetch('http://puzzle.mead.io/puzzle').then((response) => {
-//      response.json().then((data) => {
-//         console.log(data);
-//      });
-//  });
-
-//  const address = 'Valenzuela';
-//  const geoUrl = 'http://api.mapbox.com/geocoding/v5/mapbox.places/'+address+'.json?access_token=pk.eyJ1IjoiYWdtYXIyOCIsImEiOiJja2Y4MTh4cnYwN3VvMnNyMHJwb2pkcGIxIn0.3lwXLZh9dllvZeRp9JNkhQ&limit=1'
-//  const forecastUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=14.86667&lon=-118.2439&appid=7c1e5c1c2bcc0f93d029be170e3e9667&units=metric'
-
-
-//  fetch(geoUrl).then((response) => {
-//      response.json().then((data) => {
-//          const location = data.features[0].place_name
-//          const latitude = data.features[0].center[1]
-//          const longitude = data.features[0].center[0]
-        
-//          fetch('https://api.openweathermap.org/data/2.5/onecall?lat='+latitude+'&lon='+longitude+'&appid=7c1e5c1c2bcc0f93d029be170e3e9667&units=metric')
-//             .then((response) => {
-//                response.json().then((data) => {
-//                   console.log('Location: ' + location);
-//                   console.log('Forecast: ' + data.current.weather[0].description);
-//             });
-//          });
-//      });
-//   });
-
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input');
 const messageOne = document.querySelector('#message-1');
