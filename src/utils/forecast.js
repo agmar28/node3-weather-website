61a2c2655b5a4e03d5568f82bfe176f457db3d8e
agmar28 <ag.marcelo28@gmail.com>
2020-10-01 21:44:33 +0800

    Added new data to forecast endpoint

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,22 +1,4 @@
 const request = require ('request');
-
-// const forecast = (latitude, longitude, callback) => {
-//     const url = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + latitude + '&lon=' + longitude + '&appid=7c1e5c1c2bcc0f93d029be170e3e9667&units=metric'
-//     request({url: url, json: true}, (error, response) => {
-//         if (error) {
-//             callback('Unable to connect to weather service!', undefined);
-//         }else if (response.body.cod) {
-//            // callback('Invalid coordinate! Try another.', undefined);
-//            callback(error, undefined);
-//         }else {
-//             callback(undefined, {
-//                 Forecast: 'It is ' + response.body.current.weather[0].description 
-//                             + ' today!'
-//             });
-//         }
-//     });
-// };
-
 const forecast = (latitude, longitude, callback) => {
     const url = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + latitude + '&lon=' + longitude + '&appid=7c1e5c1c2bcc0f93d029be170e3e9667&units=metric'
 
@@ -29,7 +11,8 @@ const forecast = (latitude, longitude, callback) => {
         }else {
             callback(undefined, {
                 Forecast: 'It is ' + body.current.weather[0].description 
-                            + ' today!'
+                            + ' today!',
+                Temperature: 'The temperature for today is '+ body.hourly[0].temp + ' degrees celcius.'
             });
         }
     });
